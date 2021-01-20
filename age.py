d675a03ae7f0c8d71c21f8937665b53ba3c52e84
Barthos Máté <71081352+barthosmate@users.noreply.github.com>
2021-01-20 17:24:32 +0100

    Add files via upload

---
+++ age.py
@@ -0,0 +1,23 @@
+def base():
+	
+	ageList = []
+	for i in range ( 0 , 3 ):
+		age = int( input( "Kor: " ))
+		ageList.append ( age )
+		
+	ageAverage ( ageList ) 
+	
+	average = ageAverage ( ageList )
+	print( " A korok átlaga : {:>10.2f} év".format ( average ))
+	
+def ageAverage( ageList ):
+	
+	#average = ( ageList[0] + ageList[1] + ageList[2] ) / len( ageList )
+	sumAge = 0
+	for i in range(len(ageList)):
+		sumAge += ageList[i]
+	
+	average = sumAge / len(ageList)	
+	return average
+		
+base()
